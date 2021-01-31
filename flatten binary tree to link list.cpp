0c6630aaa8cbc0ef567d5fe494e689e50fe1c49a
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-01-31 17:04:48 +0530

    Create flatten binary tree to link list.cpp

---
+++ flatten binary tree to link list.cpp
@@ -0,0 +1,24 @@
+
+ // just using preorder traversal in O(n) time and O(n) space
+ // this is not efficient
+ 
+   vector<TreeNode*> temp;
+    void preorder(TreeNode* node)
+    {
+        if(node!=nullptr)
+        {
+            temp.push_back(node);
+            preorder(node->left);
+            preorder(node->right);
+        }
+    }
+    void flatten(TreeNode* root) {
+        preorder(root);
+        for(int i=0;i<int(temp.size())-1;i++)
+        {
+            temp[i]->left=nullptr;
+            temp[i]->right=temp[i+1];
+        }
+    
+    }
+    
