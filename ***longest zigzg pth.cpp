1622de1f01b5c7301182683db16983abecd832a9
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-03-17 14:48:50 +0530

    Create ***longest zigzg pth.cpp

---
+++ ***longest zigzg pth.cpp
@@ -0,0 +1,19 @@
+
+  int maxStep = 0;
+     void dfs(TreeNode* root, bool isLeft, int step) {
+        if (!root) return;
+        maxStep = max(maxStep, step); // update max step sofar
+        if (isLeft) {
+            dfs(root->left, false, step + 1); // keep going from root to left
+            dfs(root->right, true, 1); // restart going from root to right
+        } else {
+            dfs(root->right, true, step + 1); // keep going from root to right
+            dfs(root->left, false, 1); // restart going from root to left
+        }
+    }
+    int longestZigZag(TreeNode* root) {
+     
+        dfs(root, true, 0);
+        dfs(root, false, 0);
+        return maxStep;
+    }
