706cfb512fc67c9b5a918b64be05e23ee06549f6
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-06-27 17:51:18 +0530

    Create serialize and desriealize.cpp

---
+++ serialize and desriealize.cpp
@@ -0,0 +1,38 @@
+// istringstream is for input, ostringstream for output. 
+//stringstream is input and output. You can use stringstream pretty much everywhere.
+
+public:
+
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserialize(in);
+    }
+
+private:
+
+    void serialize(TreeNode* root, ostringstream& out) {
+        if (root) {
+            out << root->val << ' ';
+            serialize(root->left, out);
+            serialize(root->right, out);
+        } else {
+            out << "# ";
+        }
+    }
+
+    TreeNode* deserialize(istringstream& in) {
+        string val;
+        in >> val;
+        if (val == "#")
+            return nullptr;
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
