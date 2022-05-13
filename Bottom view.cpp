81e019930c94c61560e83ba156fb3d6f23d4282e
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2022-05-13 09:40:21 +0530

    Update Bottom view.cpp

--- Bottom view.cpp
+++ Bottom view.cpp
@@ -1,3 +1,34 @@
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        vector<int> ans; 
+        if(root == NULL) return ans; 
+        map<int,int> mpp; 
+        queue<pair<Node*, int>> q; 
+        q.push({root, 0}); 
+        while(!q.empty()) {
+            auto it = q.front(); 
+            q.pop(); 
+            Node* node = it.first; 
+            int line = it.second; 
+            mpp[line] = node->data; 
+            
+            if(node->left != NULL) {
+                q.push({node->left, line-1}); 
+            }
+            if(node->right != NULL) {
+                q.push({node->right, line + 1}); 
+            }
+            
+        }
+        
+        for(auto it : mpp) {
+            ans.push_back(it.second); 
+        }
+        return ans;  
+    }
+
+
 /*Method 1 – Using Queue 
 The following are steps to print Bottom View of Binary Tree. 
 1. We put tree nodes in a queue for the level order traversal. 
